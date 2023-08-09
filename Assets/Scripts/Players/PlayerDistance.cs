3730b7b7cc33c15af41e4c6b3a078cbba017e0be
RENATO PEREIRA <renato.innocenti@gmail.com>
2023-08-09 13:26:50 -0300

    Drop Item and Player Distance
    Script do chat GPT para corrigir

--- Assets/Scripts/Players/PlayerDistance.cs
+++ Assets/Scripts/Players/PlayerDistance.cs
@@ -5,7 +5,65 @@ namespace RiverAttack
     [RequireComponent(typeof(PlayerMaster))]
     public class PlayerDistance : MonoBehaviour
     {
-        [SerializeField] float cadenceDistance;
+        public float conversaoXParaY = 10.0f; // Valor de conversão
+    public string unidadeDeMedida = "KM"; // Unidade de medida
+    public float offsetInicial = 0.0f; // Offset para o início da distância
+
+    Vector3 lastPosition;
+    float distanciaPercorridaZ;
+    float distanciaConvertida;
+    float maxDistanciaPercorridaZ; // Ponto mais distante alcançado
+
+    const string MaxDistanciaKey = "MaxDistanciaKey"; // Chave para o valor máximo da distância
+
+    void Start()
+    {
+        lastPosition = transform.position;
+        distanciaPercorridaZ = offsetInicial;
+        LoadMaxDistancia();
+    }
+
+    void Update()
+    {
+        // Calcula a distância percorrida no eixo Z desde o último frame
+        float distanciaZ = Mathf.Abs(transform.position.z - lastPosition.z);
+
+        // Verifica se a distância é positiva (movimento para frente)
+        if (distanciaZ > 0)
+        {
+            // Adiciona a distância ao registro total
+            distanciaPercorridaZ += distanciaZ;
+
+            // Atualiza o ponto mais distante alcançado
+            maxDistanciaPercorridaZ = Mathf.Max(maxDistanciaPercorridaZ, distanciaPercorridaZ);
+        }
+
+        // Calcula a distância convertida com base na conversão configurada
+        distanciaConvertida = maxDistanciaPercorridaZ / conversaoXParaY;
+
+        // Atualiza a posição anterior
+        lastPosition = transform.position;
+
+        // Exibe a distância percorrida e convertida no console
+        Debug.Log($"Distância percorrida no eixo Z: {distanciaPercorridaZ} unidades | Distância convertida: {distanciaConvertida} {unidadeDeMedida}");
+    }
+
+    void OnApplicationQuit()
+    {
+        SaveMaxDistancia();
+    }
+
+    private void SaveMaxDistancia()
+    {
+        PlayerPrefs.SetFloat(MaxDistanciaKey, maxDistanciaPercorridaZ);
+        PlayerPrefs.Save();
+    }
+
+    void LoadMaxDistancia()
+    {
+        maxDistanciaPercorridaZ = PlayerPrefs.GetFloat(MaxDistanciaKey, offsetInicial);
+    }
+        /*[SerializeField] float cadenceDistance;
         GamePlayManager m_GamePlayManager;
         PlayerMaster m_PlayerMaster;
         int distanceOffset { get; set; }
@@ -61,6 +119,6 @@ namespace RiverAttack
         void ClearDistance()
         {
             pathDistance = (int)(transform.position.z / cadenceDistance) - distanceOffset;
-        }
+        }*/
     }
 }
